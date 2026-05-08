37217557fb84e047a6580c9f94921bfb4d20ac68
Jhon Mekier Bayan <icecrackers7@gmail.com>
2026-05-08 23:46:06 +0800

    Fix STATICFILES_STORAGE for cloudinary compatibility

--- config/settings.py
+++ config/settings.py
@@ -162,3 +162,6 @@ EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True').lower() == 'true'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER or 'noreply@agripet.local')
+
+# Add this line anywhere in settings.py
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
