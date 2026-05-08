b65ac59165c89f798deee8df2aa9898dbc37b54f
Jhon Mekier Bayan <icecrackers7@gmail.com>
2026-05-09 00:11:21 +0800

    fix: correct Cloudinary env variable names in settings

--- config/settings.py
+++ config/settings.py
@@ -151,9 +151,9 @@ STATICFILES_STORAGE = "cloudinary_storage.storage.StaticCloudinaryStorage"
 
 import os
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('daowwdnx0'),
-    'API_KEY': os.environ.get('477363789913732'),
-    'API_SECRET': os.environ.get('CY2KL2YYdkmomaWrUEPmTuTXMAU'),
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
